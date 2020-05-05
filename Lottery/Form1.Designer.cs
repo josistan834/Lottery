492fa3173348316be84b28d85606f0e69a145e76
Josiah Stanley <josistan834@ed.amdsb.ca>
2020-05-05 16:49:44 -0400

    Add project files.

---
+++ Lottery/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.genButton = new System.Windows.Forms.Button();
+            this.outputLable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.ForestGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(-2, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lottery Number Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(562, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Press the below button to generate your winning numbers";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // genButton
+            // 
+            this.genButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.genButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genButton.Location = new System.Drawing.Point(197, 147);
+            this.genButton.Name = "genButton";
+            this.genButton.Size = new System.Drawing.Size(179, 82);
+            this.genButton.TabIndex = 2;
+            this.genButton.Text = "Generate";
+            this.genButton.UseVisualStyleBackColor = false;
+            this.genButton.Click += new System.EventHandler(this.genButton_Click);
+            // 
+            // outputLable
+            // 
+            this.outputLable.BackColor = System.Drawing.Color.Black;
+            this.outputLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLable.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.outputLable.Location = new System.Drawing.Point(129, 300);
+            this.outputLable.Name = "outputLable";
+            this.outputLable.Size = new System.Drawing.Size(314, 48);
+            this.outputLable.TabIndex = 3;
+            this.outputLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(145, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Your winnning numbers are:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 367);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputLable);
+            this.Controls.Add(this.genButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Lotto Max 100000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button genButton;
+        private System.Windows.Forms.Label outputLable;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Lottery/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int num1, num2, num3, num4, num5, num6;
+        Random rand = new Random();
+
+        private void genButton_Click(object sender, EventArgs e)
+        {
+            num1 = rand.Next(1, 50);
+            do
+            {
+                num2 = rand.Next(1, 50);
+            }
+            while (num2 == num1);
+            do
+            {
+                num3 = rand.Next(1, 50);
+            }
+            while (num3 == num1 || num3 == num2);
+            do
+            {
+                num4 = rand.Next(1, 50);
+            }
+            while (num4 == num1 || num4 == num2 || num4 == num3);
+            do
+            {
+                num5 = rand.Next(1, 50);
+            }
+            while (num5 == num1 || num5 == num2 || num5 == num3 || num5 == num4);
+            do
+            {
+                num6 = rand.Next(1, 50);
+            }
+            while (num6 == num1 || num6 == num2 || num6 == num3 || num6 == num4 || num6 == num5);
+            outputLable.Text = $"{num1}, {num2}, {num3}, {num4}, {num5}, {num6}";
+        }
+    }
+}
